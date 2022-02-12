ed600efd41bee5a55d9b14be1677b4a51f1eb22a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-02-12 17:48:14 +0100

    feat: add http service

---
+++ Proxy/SonarQubeProxy/Services/HttpClientService/Authentication/BasicAuthentication.cs
@@ -0,0 +1,12 @@
+namespace SonarQubeProxy.Services.HttpClientService.Authentication;
+
+using System.Diagnostics.CodeAnalysis;
+using Abstractions;
+
+[ExcludeFromCodeCoverage]
+public class BasicAuthentication : IAuthentication
+{
+    public string Login { get; set; } = "";
+
+    public string Password { get; set; } = "";
+}
---
+++ Proxy/SonarQubeProxy/Services/HttpClientService/Authentication/BearerAuthentication.cs
@@ -0,0 +1,10 @@
+namespace SonarQubeProxy.Services.HttpClientService.Authentication;
+
+using System.Diagnostics.CodeAnalysis;
+using Abstractions;
+
+[ExcludeFromCodeCoverage]
+public class BearerAuthentication : IAuthentication
+{
+    public string Token { get; set; } = "";
+}
