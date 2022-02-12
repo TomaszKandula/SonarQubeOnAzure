68ec0a669c91d3fb2ce73eecad51b1f29e06a994
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-02-12 17:31:26 +0100

    feat: add exceptions

---
+++ Proxy/SonarQubeProxy/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿namespace SonarQubeProxy.Exceptions;
+
+using System;
+using System.Runtime.Serialization;
+using System.Diagnostics.CodeAnalysis;
+using FluentValidation.Results;
+
+[ExcludeFromCodeCoverage]
+[Serializable]
+public class ValidationException : BusinessException
+{
+    public ValidationResult ValidationResult { get; }
+
+    protected ValidationException(SerializationInfo serializationInfo, 
+        StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+
+    public ValidationException(ValidationResult validationResult, string errorMessage = "") : base(errorMessage)
+        => ValidationResult = validationResult;
+}
