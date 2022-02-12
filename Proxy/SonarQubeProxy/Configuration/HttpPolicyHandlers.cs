f388d6ec5e06ac60e700434b50da42ab2d5759eb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-02-12 17:46:47 +0100

    feat: add configuration

---
+++ Proxy/SonarQubeProxy/Configuration/HttpPolicyHandlers.cs
@@ -0,0 +1,30 @@
+namespace SonarQubeProxy.Configuration;
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using Polly.Extensions.Http;
+using Polly;
+
+[ExcludeFromCodeCoverage]
+public static class HttpPolicyHandlers
+{
+    public static IAsyncPolicy<HttpResponseMessage> SetupRetry()
+    {
+        const int retryCount = 3;
+        const double durationBetweenRetries = 150;
+
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .Or<TaskCanceledException>()
+            .OrResult(response => response.StatusCode 
+                is HttpStatusCode.RequestTimeout 
+                or HttpStatusCode.BadGateway 
+                or HttpStatusCode.GatewayTimeout 
+                or HttpStatusCode.ServiceUnavailable
+            ).WaitAndRetryAsync(retryCount, count 
+                => TimeSpan.FromMilliseconds(durationBetweenRetries * Math.Pow(2, count - 1)));
+    }
+}
